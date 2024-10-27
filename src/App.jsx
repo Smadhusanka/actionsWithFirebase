71bce417fef7388e7571e0cfee05e01d6bb08390
Smadhusanka <smadusanka2001@gmail.com>
2024-10-27 20:53:37 +0530

    change yml file and fix issue of firebase json file

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          hello
+          Welcome! React Users.....
         </p>
       </div>
       <p className="read-the-docs">
