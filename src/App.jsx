b4d4594ce3234f80d9ae7591aa5770c7c441236e
Smadhusanka <smadusanka2001@gmail.com>
2024-10-29 16:25:48 +0530

    change para

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Welcome! users
+          Welcome!
         </p>
       </div>
       <p className="read-the-docs">
